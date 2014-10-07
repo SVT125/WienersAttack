b5db7fea110d3040fadddea9249d89e80a5978bc
SVT125 <Jhardjadinata14@gmail.com>
2014-10-07 13:16:05 -0700

    removed padding methods, fully implemented RSA
    check for decryption error + other faults not yet implemented

--- WienersAttack.java
+++ WienersAttack.java
@@ -1,95 +1,88 @@
+// Notes: RSA fully works, can rewrite all as one method, half-implemented the check for e*d. Start on Wiener's attack.
+
 import java.math.BigInteger;
 import java.util.Random;
+import java.util.Arrays;
 
 public class WienersAttack {
 	public static void main( String[] args ) {
-		
+		RSA obj = new RSA(8);
+		BigInteger x = obj.generateRandomZPrime();
+		System.out.println("Encrypted...");
+		System.out.println("Cipher: " + x);
+		boolean isSecure = false;
+		BigInteger d = obj.generateDecryptionExponent(obj.e,obj.phiN);
+		BigInteger cipher = obj.encrypt(x);
+		/*
+		while( !isSecure ) {
+			d = obj.generateDecryptionExponent(obj.e,obj.phiN);
+			BigInteger check = cipher.pow((int)d.longValue()).modPow(obj.e,obj.n);
+			System.out.println("The check is: " + check); // erase
+			if(check.equals(cipher))
+				isSecure = true;
+		}
+		*/
+		System.out.println("Found d...");
+		BigInteger key = obj.decrypt(cipher,d,obj.n);
+		System.out.println("Decrypted...");
+		System.out.println("The recovered key is: " + key);
 	}
 }
 
 class RSA {
 	private int numBits;
-	public BigInteger e = new BigInteger("65537");
-	private BigInteger d,nm phiN;
+	public BigInteger e = new BigInteger("65537"), phiN, n;
+	private BigInteger d;
 	
 	public RSA(int bits) {
 		this.numBits = bits;
 		Random r = new Random();
 		BigInteger p = new BigInteger(bits,48,r), q = new BigInteger(bits,48,r);
 		this.n = p.multiply(q);
-		this.phiN = p.subtract("1").multiply.(q.subtract("1"));	
+		this.phiN = this.n.subtract(p).subtract(q).add(BigInteger.valueOf(1));	
+	}
+	// Returns a random element in Z*n.
+	public BigInteger generateRandomZPrime() {
+		BigInteger element = null;
+		boolean foundPrimeElement = false;
+		Random r = new Random();
+		while(!foundPrimeElement) {
+			BigInteger guess = new BigInteger(this.numBits,48,r);
+			if(guess.compareTo(this.n) < 0) {
+				element = guess;
+				foundPrimeElement = true;
+			}
+		}
+		return element;
 	}
 	
 	// Encrypts the message with RSA.
-	public BigInteger encrypt(String message) {
-		BigInteger messageInteger = this.translateAsInteger(message);
-		BigInteger cipher = messageInteger.modPow(this.e,this.n);
+	public BigInteger encrypt(BigInteger x) {
+		BigInteger cipher = x.pow((int)this.e.longValue());
 		return cipher;
 	}
 	
 	// Decrypts the message.
-	public String decrypt(BigInteger cipher, BigInteger d, BigInteger mod) {
+	public BigInteger decrypt(BigInteger cipher, BigInteger d, BigInteger mod) {
 		BigInteger message = cipher.modPow(d,mod);
+		
+		return message;
 	}
 	
 	// Generates the decryption exponent.
-	private BigInteger generateDecryptionExponent(BigInteger e, BigInteger mod) {
-		BigInteger numerator = 1, decryptionExponent;
+	// Public access temporarily for test RSA.
+	public BigInteger generateDecryptionExponent(BigInteger e, BigInteger mod) {
+		BigInteger numerator = BigInteger.valueOf(1), decryptionExponent = null;
 		int multiple = 0;
 		boolean foundDecryptionExponent = false;
 		while(!foundDecryptionExponent) {
-			numerator = 1 + mod.multiply(multiple);
-			if(numerator.mod(e) == 0) {
+			numerator = BigInteger.valueOf(1).add(mod.multiply(BigInteger.valueOf(multiple)));
+			if(numerator.mod(e).equals(BigInteger.valueOf(0))) {
 				decryptionExponent = numerator.divide(e);
+				foundDecryptionExponent = true;
 			} else
 				multiple++;
 		}
 		return decryptionExponent;
-	}
-	
-	// Goes per character in the message, translating each to an integer then concatenating.
-	private BigInteger translateAsInteger(String message) {
-		String buffer = "";
-		char[] messageArray = message.toCharArray();
-		for( char c : messageArray )
-			buffer = buffer + this.formatInteger(Character.getNumericValue(c));
-
-		BigInteger messageInteger = new BigInteger(buffer);
-		return messageInteger;
-	}
-	
-	// Goes per digit in the message, translating each to a character then concatenating to a string.
-	private String translateAsString(BigInteger message) {
-		char[] messageArray = message.toString().toCharArray();
-		for( char c : messageArray ) {
-			
-		}
-	}
-	
-	// Pads the given integer with 0's to allow formatting.
-	// Works for values < 1000, 4 digit integers.
-	private String formatInteger(int element) {
-		String strRep = Integer.toString(element);
-		if(element < 10)
-			return strRep + "00";
-		else if(element < 100)
-			return strRep + "0";
-		else if(element < 1000)
-			return strRep;
-		
-	}
-	
-	// Strips the given block of the 0 padding.
-	private String stripPadding(String block) {
-		char[] charArray = block.toCharArray();
-		boolean foundEnd = false;
-		int index = 0, endIndex;
-		while(!foundEnd) {
-			if(charArray[index] == '0') {
-				foundEnd = true;
-				endIndex = index;
-			} else
-				index++;
-		}
 	}
 }
